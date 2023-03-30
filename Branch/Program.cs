7b962e7fa7317150ec7d8975d2c9a5bcdc2260f3
macky <macky@DESKTOP-CENLHJ3>
2023-03-30 23:31:18 +0800

    Add project files.

---
+++ Branch/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+class Branch
+
+{
+
+  
+    public static void Main(String[] args)
+    {
+
+        int i;
+        string s;
+
+
+        List<Branch> branches = new List<Branch>();
+
+        for(i = 1; i<= 5; i++)
+        {
+            //     Console.WriteLine(branches.Count);
+            Console.WriteLine("\nEnter branch: ");
+            Console.ReadLine(); branches.Add(new Branch());
+
+        }
+
+    }
+
+}
